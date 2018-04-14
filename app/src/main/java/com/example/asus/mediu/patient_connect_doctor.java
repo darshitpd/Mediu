97589c33657b7e06be0d915031c4e80cdb1e22c1
devanshgandhi <devanshgandhi3@gmail.com>
2018-04-14 21:04:34 +0530

    updated 04/14/2018

---
+++ app/src/main/java/com/example/asus/mediu/patient_connect_doctor.java
@@ -0,0 +1,40 @@
+package com.example.asus.mediu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class patient_connect_doctor extends AppCompatActivity {
+
+    private TextView openconnecteduserslist,openuserlist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_connect_doctor);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Connections");
+
+
+        openconnecteduserslist = (TextView) findViewById(R.id.action_openconnecteduserslist);
+        openuserlist = (TextView) findViewById(R.id.action_openuserlist);
+
+        openuserlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),UsersList.class));
+            }
+        });
+
+
+        openconnecteduserslist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),ConnectedDoctorList.class));
+            }
+        });
+
+    }
+}
